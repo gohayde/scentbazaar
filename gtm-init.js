d062e3b4a10a5a296a17ea8cf393da37d823ba22
gohayde <haydeagency@gmail.com>
2026-05-06 02:35:10 +0400

    Reduce homepage startup network work

--- gtm-init.js
+++ gtm-init.js
@@ -1,4 +1,18 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-B3WP9WJW4L');
+function loadGoogleAnalytics(){
+  if (document.querySelector('script[data-analytics-id="G-B3WP9WJW4L"]')) return;
+  var script = document.createElement('script');
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-B3WP9WJW4L';
+  script.async = true;
+  script.dataset.analyticsId = 'G-B3WP9WJW4L';
+  script.onload = function(){
+    gtag('js', new Date());
+    gtag('config', 'G-B3WP9WJW4L');
+  };
+  document.head.appendChild(script);
+}
+window.addEventListener('load', function(){
+  var schedule = window.requestIdleCallback || function(callback) { return setTimeout(callback, 2500); };
+  schedule(loadGoogleAnalytics, { timeout: 5000 });
+}, { once: true });
