7e4433f120d2e7acabb337acd08be0a9cb6f0566
gohayde <haydeagency@gmail.com>
2026-05-08 12:28:29 +0400

    Improve homepage PageSpeed performance

--- gtm-init.js
+++ gtm-init.js
@@ -1,11 +1,18 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-B3WP9WJW4L');
 
 (function(){
-  var s = document.createElement('script');
-  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-B3WP9WJW4L';
-  s.async = true;
-  document.head.appendChild(s);
+  function loadGtag() {
+    if (document.querySelector('script[src*="googletagmanager.com/gtag/js"]')) return;
+    gtag('js', new Date());
+    gtag('config', 'G-B3WP9WJW4L');
+    var s = document.createElement('script');
+    s.src = 'https://www.googletagmanager.com/gtag/js?id=G-B3WP9WJW4L';
+    s.async = true;
+    document.head.appendChild(s);
+  }
+  window.addEventListener('load', function() {
+    var schedule = window.requestIdleCallback || function(cb) { return setTimeout(cb, 1500); };
+    schedule(loadGtag, { timeout: 4000 });
+  }, { once: true });
 })();
